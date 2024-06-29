e22baec35dd76efca1de0692801bddea887615df
Yuvraj Anand <contactyuvrajanand@gmail.com>
2024-06-29 10:31:49 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert(3+3) //This will return a value only when used in browser, and we are using Nodejs
+
+//Primitive Data types
+
+let age= 20 //number
+let name="Yuvraj" //string
+let isLoggedIn= true //boolean
+let state; //undefined
+
+//number => Range: 2^53   
+//BigInt => Very large range
+//boolean
+//String => Can be used in both "" & '', but prefer using ""
+//null => This data type can hold only one possible value that is null.
+//undefined => value of a variable not defined
+//symbol => Symbol data type is used to create objects which will always be unique.
+
+//Non-primitive data types
+
+//object
+//array
+
+console.log(typeof 8) //output: number
+console.log(typeof "Yuvraj") //output: object
+console.log(typeof null) //output: object
+console.log(typeof undefined) //output: undefined
+console.log(typeof true) //output: boolean
